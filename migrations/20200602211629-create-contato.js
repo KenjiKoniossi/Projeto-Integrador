477adf53507bcfc747227e965395e07967848acd
Wesley Soares <wesleybs90@gmail.com>
2020-06-02 18:22:31 -0300

    Adicionado migration para form contato, tabela contato

---
+++ migrations/20200602211629-create-contato.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('contato', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      mensagem: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('contato');
+  }
+};
---
+++ models/contato.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Contato = sequelize.define('Contato', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    nome: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    mensagem: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+  }, {
+    tableName: 'contato'
+  });
+  Contato.associate = function(models) {
+    // associations can be defined here
+  };
+  return Contato;
+};
