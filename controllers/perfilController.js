91ad81c4867d7af07f740e3d696ceee02d30c337
Wesley Soares <wesleybs90@gmail.com>
2020-06-07 19:19:39 -0300

    Adicionado o controller  inicialpara a página do perfil do usuário e modificaçõa view

--- controllers/perfilController.js
+++ controllers/perfilController.js
@@ -1,6 +1,32 @@
+const {Perfil, Problema} = require('../models');
+
 const perfilController = {
-    perfil: (req, res) => {
-        res.render('perfil')
+    perfil: async (req, res) => {
+        //Verifica se o usuário já tem um perfil
+        let perfilUsuario = await Perfil.findOne({
+            where: {
+                usuario_id: req.session.usuario.id
+            }
+        });
+
+        //Se tiver um perfil, recupera a descrição do perfil, se não, adiciona um frase padrão
+        let sobre_usuario = 'Adicione aqui uma informações sobre você. Clique em "Atualizar perfil" para editar suas informações.';
+        if (perfilUsuario !== null) {
+            sobre_usuario = perfilUsuario.sobre_usuario;
+        }
+
+        //Verifica se o usuário já enviou problemas
+        let listaProblemas = await Problema.findAll({
+            where: {
+                usuario_id: req.session.usuario.id
+            }
+        });
+        //Trata o objeto Sequelize instances, convertendo para JSON
+        listaProblemas = JSON.stringify(listaProblemas);
+        //Trata o JSON para array
+        listaProblemas = JSON.parse(listaProblemas);
+        
+        res.render('perfil', {sobre_usuario, listaProblemas});
     },
     atualizarPerfil: (req, res) => {
         res.render('atualizarPerfil')
