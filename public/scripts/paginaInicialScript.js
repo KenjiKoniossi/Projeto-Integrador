15f70469f2aeeedd7564ecfe419b2a35261ca338
Wesley Soares <wesleybs90@gmail.com>
2020-06-15 00:56:26 -0300

    Adicionado validação front-end com regex para newsletter com alerta de input inválido

--- public/scripts/paginaInicialScript.js
+++ public/scripts/paginaInicialScript.js
@@ -1,12 +1,35 @@
 let botaoNewsletter = document.querySelector('#form-newsletter button');
 let emailNewsletter = document.getElementById('email-newsletter');
+let textoErro = document.getElementById('erro-newsletter');
+
+// let textoErro = document.createElement('p');
+// textoErro.setAttribute('id', 'erro-newsletter');
+
+
+emailNewsletter.addEventListener('keypress', function () {
+
+    emailNewsletter.style.borderColor = '';
+
+    //Valida se é um email
+    let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regexEmail.test(emailNewsletter.value)) {
+        textoErro.innerHTML = 'Email inválido';
+        emailNewsletter.after(textoErro);
+    } else {
+        textoErro.innerHTML = '';
+    }
+
+})
+
 
 botaoNewsletter.addEventListener('click', async function (event) {
     event.preventDefault();
 
     //Valida se o campo foi preenchido, retorna erro caso contrário
-
-    //Valida se é um email
+    if (emailNewsletter.value === '') {
+        emailNewsletter.style.borderColor = 'red';
+        return;
+    }
 
     //Solicita o request de envio dos dados
     const resposta = await fetch('/newsletter', {
