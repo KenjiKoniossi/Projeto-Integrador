746c30fdf7f24317a088367f71b515f5c22cc98e
Wesley Soares <wesleybs90@gmail.com>
2020-06-21 19:38:10 -0300

    Adicionado marcadores no mapa e CSS para o mapa, correções nna página inicial e CSS da mesma

--- public/scripts/mapaScript.js
+++ public/scripts/mapaScript.js
@@ -2,25 +2,50 @@ const inputPesquisa = document.getElementById('input-pesquisa');
 const submitPesquisa = document.getElementById('submit-pesquisa');
 const resultadoPesquisa = document.getElementById('resultado-pesquisa');
 
+//Array de marcadores
+let arrayMarcadores = [];
+let arrayDivResultados = [];
+
 //Criando mapa com LeafletJS
 const mapa = L.map('div-mapa', {zoomControl: false}).setView([-23.5506507, -46.6333824], 17);
 
 //Usando Tiles do OpenStreetMap
-// const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
-// const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-// const tiles = L.tileLayer(tileUrl, { attribution });
-// tiles.addTo(mapa);
+function tileOpenStreetMap() {
+    const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    return tiles;
+}
 
 //Usando Tiles do Mapbox
-const tileUrl = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1Ijoid2VzbGV5YnMwMDEiLCJhIjoiY2tibmI4NmNnMXBjajJzazBvZXNoOTQ2MyJ9.bVWk-q7qy71I279L4BEv6Q'
-}).addTo(mapa);
+function tileMapbox() {
+    const tileUrl = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/light-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1Ijoid2VzbGV5YnMwMDEiLCJhIjoiY2tibmI4NmNnMXBjajJzazBvZXNoOTQ2MyJ9.bVWk-q7qy71I279L4BEv6Q'
+    });
+    return tileUrl;
+}
 
+
+//Usando Tiles do Maptiler
+function tileMaptiler() {
+    let gl = L.mapboxGL({
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
+        accessToken: 'not-needed',
+        style: 'https://api.maptiler.com/maps/e8c1f31f-ffea-44fa-9cdc-e117fbc11ceb/style.json?key=rOWMCvQS4fuQOnVJzQEQ'
+    });
+    return gl;
+}
+
+//Escolha do estilo do mapa, inserir a função para um dos estilos acima
+const tileMapa = tileMaptiler().addTo(mapa);
+
+
+//Adiciona botões de zoom no canto direito inferior
 L.control.zoom({
     position:'bottomright'
 }).addTo(mapa);
@@ -38,13 +63,13 @@ pegarGeolocation();
 
 inputPesquisa.addEventListener('keydown', async function (e) {
 
-    const resposta = await fetch('https://nominatim.openstreetmap.org/search/' + inputPesquisa.value + '?format=json&limit=5');
-    const dados = await resposta.json();
+    // const resposta = await fetch('https://nominatim.openstreetmap.org/search/' + inputPesquisa.value + '?format=json&limit=5');
+    // const dados = await resposta.json();
 
-    console.log("Digitado: " + inputPesquisa.value);
-    for (let i = 0; i < dados.length; i++) {
-        console.log(dados[i].display_name);
-    }
+    // console.log("Digitado: " + inputPesquisa.value);
+    // for (let i = 0; i < dados.length; i++) {
+    //     console.log(dados[i].display_name);
+    // }
 
     //Assim que começar a digitar carrega a div com o texto "Carregando"
 
@@ -69,6 +94,13 @@ submitPesquisa.addEventListener("click", async function (event) {
         return;
     }
 
+    //Limpa marcadores da tela
+    for (let i = 0; i < arrayMarcadores.length; i++) {
+        mapa.removeLayer(arrayMarcadores[i]);
+    }
+    arrayMarcadores = [];
+    arrayDivResultados = [];
+
     //Solicita o request de envio dos dados
     const resposta = await fetch('/mapa/pesquisa', {
         headers: {
@@ -82,10 +114,14 @@ submitPesquisa.addEventListener("click", async function (event) {
 
     if (resposta.status !== 200) {
 
+        //Limpa os resultados
+        resultadoPesquisa.innerHTML = '';
+
         //Caso retorne erro, mostrar frase
         let divConteudo = document.createElement('div');
-        divConteudo.classList.add('col-12', 'div-problemas');
+        divConteudo.classList.add('col-12', 'd-flex', 'flex-wrap', 'div-problemas');
         let pConteudo = document.createElement('p');
+        pConteudo.classList.add('col-12');
         pConteudo.innerText = 'Algo deu errado, recarregue a página e tente novamente.'
         divConteudo.appendChild(pConteudo);
         resultadoPesquisa.appendChild(divConteudo);
@@ -100,36 +136,62 @@ submitPesquisa.addEventListener("click", async function (event) {
 
             for (let i = 0; i < dadosBusca.buscaRua.length; i++) {
 
-                let divConteudo = document.createElement('div');
-                divConteudo.classList.add('col-12', 'div-problemas');
-                let h5Conteudo = document.createElement('h5');
-
-                h5Conteudo.innerText = dadosBusca.buscaRua[i].tag.tag;
-
-                let pConteudo = document.createElement('p');
-
+                //Se a descrição for longa, encurta
+                let descricaoCurta = '';
                 if (dadosBusca.buscaRua[i].descricao.length > 0) {
                     if (dadosBusca.buscaRua[i].descricao.length > 30) {
-                        pConteudo.innerText = dadosBusca.buscaRua[i].descricao.split(0, 29) + '...';
+                        descricaoCurta = dadosBusca.buscaRua[i].descricao.slice(0, 35) + '...';
                     } else {
-                        pConteudo.innerText = dadosBusca.buscaRua[i].descricao;
+                        descricaoCurta = dadosBusca.buscaRua[i].descricao;
                     }
                 } else {
-                    pConteudo.innerText = 'Problema sem descrição.';
+                    descricaoCurta = 'Problema sem descrição.';
                 }
 
-                divConteudo.appendChild(h5Conteudo);
-                divConteudo.appendChild(pConteudo);
-                resultadoPesquisa.appendChild(divConteudo);
+                let dataCriacao = new Date(dadosBusca.buscaRua[i].data_criacao);
 
+                let divConteudo = document.createElement('div');
+                divConteudo.classList.add('col-12', 'd-flex', 'flex-wrap', 'div-problemas');
+
+                if (i % 2 !== 0) {
+                    divConteudo.classList.add('cor-cinza');
+                }
+
+                divConteudo.innerHTML = `<p class="col-4 mb-1 p-pequena"><strong>ID: </strong>${dadosBusca.buscaRua[i].id}</p>
+                                        <p class="col-8 mb-1 p-pequena"><strong>Data: </strong>${dataCriacao.getDate()}/${dataCriacao.getMonth()}/${dataCriacao.getFullYear()}</p>
+                                        <h6 class="col-12 mb-1">${dadosBusca.buscaRua[i].tag.tag}</h6>
+                                        <p class="col-12 mb-0">${descricaoCurta}</p>`;
+
+                resultadoPesquisa.appendChild(divConteudo);
+                arrayDivResultados.push(divConteudo);
+
+                let conteudoPopup = '<h5>' + dadosBusca.buscaRua[i].tag.tag + '</h5><p>' + (dadosBusca.buscaRua[i].descricao.length > 0 ? dadosBusca.buscaRua[i].descricao : 'Problema sem descrição.') + '</p>';
+                
+                //Adiciona marcador no mapa
+                let marcador = L.marker([dadosBusca.buscaRua[i].endereco.geolocalizacao.coordinates[0], dadosBusca.buscaRua[i].endereco.geolocalizacao.coordinates[1]]).bindPopup(conteudoPopup).addTo(mapa);
+                arrayMarcadores.push(marcador);
+
+                //Centraliza o mapa no marcador ao clicar no marcador
+                arrayMarcadores[i].addEventListener("click", function (){
+                    mapa.panTo(this.getLatLng());
+                });
+                
+                arrayDivResultados[i].addEventListener("click", function (){
+                    arrayMarcadores[i].openPopup();
+                    mapa.panTo(arrayMarcadores[i].getLatLng());
+                });
             }
 
         } else {
 
+            //Limpa os resultados
+            resultadoPesquisa.innerHTML = '';
+
             //Caso retorne vazio, mostrar frase
             let divConteudo = document.createElement('div');
-            divConteudo.classList.add('col-12', 'div-problemas');
+            divConteudo.classList.add('col-12', 'd-flex', 'flex-wrap', 'div-problemas');
             let pConteudo = document.createElement('p');
+            pConteudo.classList.add('col-12');
             pConteudo.innerText = 'Não foram encontrados resultados.'
             divConteudo.appendChild(pConteudo);
             resultadoPesquisa.appendChild(divConteudo);
